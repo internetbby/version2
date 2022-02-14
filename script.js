9e212a963574d4a9bb9040e675d41ba440386eff
Linnea <linnea.alse@elev.ga.ntig.se>
2022-02-14 10:52:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,129 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+let header = document.getElementById("header");
+
+let firstGrid = document.getElementById("firstGrid");
+
+var userNumber = Math.floor(Math.random() * 5);
+
+console.log(userNumber);
+
+var userNumberString = userNumber.toString();
+
+const sessionStorageCheck = sessionStorage.getItem("userColour");
+
+if (sessionStorageCheck) {
+} else {
+  sessionStorage.userColour = userNumberString;
+} //creates a sessionstorage if no one was found
+
+if (sessionStorage.userColour == "0") {
+  header.classList.add("red");
+  navbar.classList.add("red");
+  firstGrid.classList.add("red");
+
+  console.log("default");
+}
+
+if (sessionStorage.userColour == "1") {
+  header.classList.add("blue");
+  navbar.classList.add("blue");
+  console.log("blue");
+}
+
+if (sessionStorage.userColour == "2") {
+  header.classList.add("green");
+  navbar.classList.add("green");
+  console.log("green");
+}
+
+if (sessionStorage.userColour == "3") {
+  header.classList.add("yellow");
+  navbar.classList.add("yellow");
+  console.log("yellow");
+}
+
+if (sessionStorage.userColour == "4") {
+  header.classList.add("purple");
+  navbar.classList.add("purple");
+  console.log("purple");
+}
+
+var seconds = 0;
+
+function incrementSeconds() {
+  seconds += 1;
+
+}
+
+var cancel1 = setInterval(incrementSeconds, 1000);
+var update2 = setInterval(ifOnHomePage, 1000)
+
+if (sessionStorage.getItem("aboutTimer") === null) {
+  sessionStorage.setItem("aboutTimer", 0)
+}
+if (sessionStorage.getItem("firstPageTimer") === null) {
+  sessionStorage.setItem("firstPageTimer", 0)
+}
+if (sessionStorage.getItem("storyTimer") === null) {
+  sessionStorage.setItem("storyTimer", 0)
+}
+
+
+
+function ifOnHomePage() {
+  if (document.URL.includes("index.html")) {
+    var intHolder = parseInt(sessionStorage.firstPageTimer);
+    intHolder += seconds;
+
+    sessionStorage.setItem("firstPageTimer", intHolder);
+
+
+
+  }
+  if (document.URL.includes("about.html")) {
+
+
+    var intHolder = parseInt(sessionStorage.aboutTimer);
+    intHolder += seconds;
+
+
+
+    sessionStorage.setItem("aboutTimer", intHolder);
+  }
+
+  if (document.URL.includes("story.html")) {
+
+
+    var intHolder = parseInt(sessionStorage.storyTimer);
+    intHolder += seconds;
+
+
+
+    sessionStorage.setItem("storyTimer", intHolder);
+  }
+}
+
+console.log("index " + sessionStorage.firstPageTimer)
+
+console.log("about " + sessionStorage.aboutTimer)
+console.log("story "+ sessionStorage.storyTimer);
+
